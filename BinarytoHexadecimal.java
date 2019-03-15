2548a867c3e51c4e81712dd6ac90f9337902f90a
Ellis.Tran@jbhunt.com <ellisktran@gmail.com>
2019-03-14 21:43:12 -0500

    Take out string reverse

--- BinarytoHexadecimal.java
+++ BinarytoHexadecimal.java
@@ -124,19 +124,9 @@ class BinarytoHexadecimal extends Conversions {
     return this.getStringValue();
   }
 
-  private String reverseString(String number) {
-    StringBuilder sb = new StringBuilder();
-    for (int i = number.length() - 1; i >= 0; i--) {
-      sb.append(number.charAt(i));
-    }
-    return sb.toString();
-  }
-
   public void printConversion() {
     System.out.println("The conversion name is: " + this.getConversionName());
     System.out.println("The number to be converted is: " + this.getNumber1());
     System.out.println("The converted value is (Regular Method): " + this.doConversion());
-    // System.out.println("The converted value is (Stack Method): " +
-    // this.doConversionWithStack());
   }
 }
