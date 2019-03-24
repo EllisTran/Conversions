09c989d5ec44ba8231a4e955b170958dbafcdf13
Ellis.Tran@jbhunt.com <ellisktran@gmail.com>
2019-03-24 00:12:28 -0500

    Moved everything to poly style instead of abstract style to save code space

--- Java Conversions/Conversions.java
+++ Java Conversions/Conversions.java
@@ -2,31 +2,43 @@ import java.lang.Math;
 
 abstract class Conversions {
   protected String conversionName = "";
-  protected long number1 = 0;
+  protected long number = 0;
   protected long value = 0;
   protected boolean converted = false;
-
+  Conversions(long number, String conversionName) {
+    this.setNumber(number);
+    this.setConversionName(conversionName);
+  }
   // Setter and Getter Functions
-  abstract void setNumber(long num);
+  private void setNumber(long number) {
+    this.number = number;
+  }
+  protected long getNumber() {
+    return this.number;
+  }
+  protected void setConverted() {
+    this.converted = true;
+  }
+  protected boolean getConverted() {
+    return this.converted;
+  }
+  protected void setValue(long value) {
+    this.value = value;
+  }
+  protected long getValue() {
+    return this.value;
+  }
+  private void setConversionName(String conversionName) {
+    this.conversionName = conversionName;
+  }
+  protected String getConversionName() {
+    return this.conversionName;
+  }
+  protected abstract void doConversion();
 
-  abstract long getNumber1();
-
-  abstract void setConverted();
-
-  abstract boolean getConverted();
-
-  abstract void setValue(long value);
-
-  abstract long getValue();
-
-  abstract String getConversionName();
-
-  abstract void setConversionName(String conversionName);
-
-//  abstract void printConversion();
   protected void printConversion() {
     System.out.println("The conversion name is: " + this.getConversionName());
-    System.out.println("The number to be converted is: " + this.getNumber1());
+    System.out.println("The number to be converted is: " + this.getNumber());
     System.out.println("The converted value is: " + this.getValue());
   }
 }
